a2bf71ec7354ae72ac9e8a05e3b826b87f34e671
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2018-07-02 22:36:27 +0530

    initial commit

---
+++ src/Containers/Chart.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Chart extends React.Component {
+  public render() {
+    return (
+      <div className="chart">
+        Chart Container
+      </div>
+    )
+  }
+}
+
+export default Chart
