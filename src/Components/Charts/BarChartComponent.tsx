1e832820078ed6a91b7f0ae78f27fa855548b192
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2019-03-28 14:45:26 +0530

    added ability to switch between line graph and bar graph

--- src/Components/Charts/BarChartComponent.tsx
+++ src/Components/Charts/BarChartComponent.tsx
@@ -1,34 +1,47 @@
 import * as React from 'react'
 import * as moment from 'moment'
-import { axisBottom, axisLeft, scaleLinear, scaleBand, select } from 'd3'
+import { axisBottom, axisLeft, scaleLinear, scaleBand, select, event, easeLinear } from 'd3'
 
-import DatePickerComponent from './DatePicker'
 import './BarChartComponent.css'
 
 
 interface IBarProps {
   data: object[],
-  dateChange: (fromDate: moment.Moment, toDate: moment.Moment) => void
+  fetchSingleFactoryData: (date: string | Date, location: string) => Promise<void>
+}
+
+interface IDataShape {
+  PRDDATE: string,
+  EFF: number,
+  LOCATION: string
 }
 
 class BarChartComponent extends React.Component<IBarProps, {}> {
   public render() {
-    this.drawChart()
     return (
-      <div>
-        <DatePickerComponent onChange={this.props.dateChange} />
-        <div style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-          <svg className="chart"></svg>
-        </div>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <svg className="chart" />
       </div>
     )
   }
 
-  private getFactoryKeys = (data: object[]) => {
+  public componentDidMount() {
+    const { data } = this.props
+    if (data.length > 0) {
+      this.drawChart()
+    }
+  }
+
+  public componentDidUpdate() {
+    console.log("Component did update")
+    this.drawChart()
+  }
+
+  private getFactoryKeys = (data: object[]): string[] => {
     const keys: string[] = data.reduce<string[]>((acc: string[], curr: object) => {
       acc.push(...Object.keys(curr))
       return acc
@@ -41,17 +54,18 @@ class BarChartComponent extends React.Component<IBarProps, {}> {
     return [...new Set(keys)]
   }
 
-  private drawChart = () => {
+  private drawChart = (): void => {
     const { data } = this.props
     const keys = this.getFactoryKeys(data)
 
+    //  Define all the dimensions required for showing the SVG
     const height = 480
     const width = 960
-
     const margin: { top: number, right: number, bottom: number, left: number } = { top: 30, right: 30, bottom: 30, left: 30 }
 
+    //  Define the scales and the axes required
     const x = scaleBand()
-      .domain(data.map(d => d['PRDDATE']))
+      .domain(data.map((d: IDataShape) => d.PRDDATE))
       .rangeRound([0, width])
 
     const y = scaleLinear()
@@ -63,35 +77,64 @@ class BarChartComponent extends React.Component<IBarProps, {}> {
       .rangeRound([0, x.bandwidth()])
       .padding(0.5)
 
-    const xAxis = (g: any) => g.attr('transform', `translate(0, ${height})`)
-                        .call(axisBottom(x)
-                              .tickFormat(d => moment(d).format('MMMM DD, YYYY'))
-                          )
+    const xAxis = (g: any) => g.attr('transform', `translate(${margin.left - 10}, ${height + margin.top})`)
+      .call(axisBottom(x)
+        .tickFormat(d => moment(d).format('MMMM DD, YYYY'))
+      )
 
-    const yAxis = (g: any) => g.attr('transform', `translate(${margin.left}, 0)`)
-                          .call(axisLeft(y))
+    const yAxis = (g: any) => g.attr('transform', `translate(${margin.left - 5}, ${margin.top})`)
+      .call(axisLeft(y))
 
+    //  Define the tooltip
+    const tooltip = select('body')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0)
+
+    //  Select the SVG element and give dimensions
     const chart = select('.chart')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
 
-    chart.selectAll('g')
-          .data(data)
-          .join('g')
-            .attr('transform', function (d) { return `translate(${x(d['PRDDATE'])}, 0)` })
-          .selectAll('rect')
-          .data(d => keys.map(key => ({ key, value: d[key] })))
-          .join('rect')
-            .attr('x', d => x1(d.key) || null)
-            .attr('y', d => y(d.value))
-            .attr('width', x1.bandwidth())
-            .attr('height', d => height - y(d.value))
 
+    //  Draw the actual chart with the required data
     chart.append('g')
-          .call(xAxis)
+      .selectAll('g')
+      .data(data)
+      .join('g')
+      .attr('transform', function (d: IDataShape) { return `translate(${x(d.PRDDATE)! + margin.left - 10}, ${margin.top})` })
+      .selectAll('rect')
+      .data((d: IDataShape) => keys.map(key => ({ key, value: d[key], date: d.PRDDATE })))
+      .join('rect')
+      .attr('x', d => x1(d.key) || null)
+      .attr('y', d => y(d.value))
+      .attr('width', x1.bandwidth())
+      .attr('height', d => height - y(d.value))
+      .on('mouseover', d => {
+        tooltip.transition()
+          .duration(250)
+          .ease(easeLinear)
+          .style('opacity', 0.9)
+        tooltip.html(d.key + "<br />" + d.value)
+          .style("left", (event.pageX) + "px")
+          .style("top", (event.pageY - 28) + "px")
+      })
+      .on('mouseout', d => {
+        tooltip.transition()
+          .duration(250)
+          .ease(easeLinear)
+          .style('opacity', 0)
+      })
+      .on('click', (d) => {
+        this.props.fetchSingleFactoryData(d.date, d.key)
+      })
 
+    //  Append the axes on to the chart
     chart.append('g')
-          .call(yAxis)
+      .call(xAxis)
+    chart.append('g')
+      .call(yAxis)
+
   }
 }
 
---
+++ src/Components/Charts/LineChartComponent.tsx
@@ -0,0 +1,197 @@
+import * as React from 'react'
+import * as moment from 'moment'
+import { axisBottom, axisLeft, scaleLinear, scaleTime, select, line, mouse, bisector, ContainerElement } from 'd3'
+
+import './LineChartComponent.css'
+
+interface IDataShape {
+  PRDDATE: string | Date,
+  EFF: number
+}
+
+interface IBarProps {
+  data: IDataShape[],
+  fetchSingleDataPoint: (date: string | Date) => Promise<void>
+}
+
+class LineChartComponent extends React.Component<IBarProps, {}> {
+  public render() {
+    return (
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <svg className="chart" />
+      </div>
+    )
+  }
+
+  public componentDidUpdate() {
+    if (this.props.data.length > 0) {
+      this.drawChart()
+    }
+  }
+
+  public componentWillUnmount() {
+    //  Remove all DOM nodes created under the .chart svg
+    select('.chart')
+        .selectAll('g')
+        .remove()
+
+    select('.chart')
+        .selectAll('path')
+        .remove()
+
+    select('.chart')
+        .selectAll('circle')
+        .remove()
+  }
+
+  private drawChart = (): void => {
+    const { data } = this.props
+    console.log(data)
+    //  Formate date from data for use in bisector function
+    const dateFormattedData: any = data.reduce((acc: IDataShape[], currObj) => {
+      const updatedObj = Object.assign(
+        {},
+        { ...currObj },
+        { PRDDATE: moment(currObj.PRDDATE).toDate() }
+      )
+      acc.push(updatedObj)
+      return acc
+    }, [])
+
+    //  Define the dates that will be used for the x domain
+    const startDate = moment(data[0].PRDDATE).toDate()
+    const endDate = moment(data[data.length - 1].PRDDATE).toDate()
+
+    //  Define all the dimensions required for showing the SVG
+    const height = 480
+    const width = 960
+    const margin: { top: number, right: number, bottom: number, left: number } = { top: 30, right: 60, bottom: 30, left: 60 }
+
+    //  Define the scales and axes required
+    const xScale = scaleTime()
+      .domain([startDate, endDate])
+      .rangeRound([margin.left, width - margin.right])
+
+    const yScale = scaleLinear()
+      .domain([0, 100])
+      .range([height - margin.bottom, margin.top])
+
+    const xAxis = (g: any) => g.attr('transform', `translate(0, ${height - margin.bottom})`)
+      .call(axisBottom(xScale))
+
+    const yAxis = (g: any) => g.attr('transform', `translate(${margin.left}, 0)`)
+      .call(axisLeft(yScale))
+
+    //  Define the bisector function to use for finding the position of the tooltip
+    const bisectorFn = (value: number) => {
+      const bisect = bisector((d: any) => d.PRDDATE).left
+      const date = xScale.invert(value)
+      const index = bisect(dateFormattedData, date)
+      const a = dateFormattedData[index - 1]
+      const b = dateFormattedData[index]
+      try {
+        return date.getTime() - a.PRDDATE.getTime() > date.getTime() - b.PRDDATE.getTime() ? a : b
+      } catch (e) {
+        console.log(e)
+        return a || { PRDDATE: null, EFF: null }  //  return undefined || object
+      }
+    }
+
+    //  Select the SVG element and give dimensions
+    const chart = select('.chart')
+      .attr('width', width)
+      .attr('height', height)
+
+    //  Define the tooltip
+    const tooltip = chart.append('g')
+                        .attr('class', 'tooltip')
+
+    //  Define the function for the tooltip
+    const callout = (g: any, value: string) => {
+      if (!value) {
+        return g.style("display", "none")
+      }
+
+      g
+        .style("display", null)
+        .style("pointer-events", "none")
+        .style("font", "10px sans-serif");
+
+      const path = g.selectAll("path")
+        .data([null])
+        .join("path")
+        .attr("fill", "white")
+        .attr("stroke", "black");
+
+      const text = g.selectAll("text")
+        .data([null])
+        .join("text")
+        .call((t: any) => t
+        .selectAll("tspan")
+        .data((value + "").split(/\n/))
+        .join("tspan")
+        .attr("x", 0)
+        .attr("y", (d: any, i: any) => `${i * 1.1}em`)
+        .text((d: any) => d));
+
+      const { x, y, width: w, height: h } = text.node().getBBox();
+
+      text.attr("transform", `translate(${-w / 2},${15 - y})`);
+      path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+    }
+
+    //  Define the line that is going to be shown
+    const dataLine = line<IDataShape>()
+      .x((d: IDataShape) => {
+        return xScale(moment(d.PRDDATE).toDate())
+      })
+      .y((d: IDataShape) => {
+        return yScale(d.EFF)
+      })
+
+    //  Add the svg line onto the chart
+    chart.append('path')
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr('d', dataLine)
+
+    //  Add the circles at each node
+    chart.selectAll('dot')
+          .data(data)
+          .join('circle')
+          .attr('r', 5)
+          .attr('cx', (d) => xScale(moment(d.PRDDATE).toDate()))
+          .attr('cy', (d) => yScale(d.EFF))
+          .on('click', (e: IDataShape) => {
+            const { PRDDATE } = e
+            this.props.fetchSingleDataPoint(PRDDATE)
+          })
+
+    //  Add a mousemove event to the chart to display the tooltip
+    chart.on('mousemove', function (this: ContainerElement) {
+      const coordinates = mouse(this)
+      const { PRDDATE, EFF } = bisectorFn(coordinates[0])
+
+      tooltip.attr('transform', `translate(${xScale(PRDDATE)}, ${yScale(EFF)})`)
+        .call(callout, `${EFF}% \n ${PRDDATE.toDateString()}`)
+      })
+      .on('mouseleave', function() {
+        tooltip.call(callout, null)
+      })
+
+
+    //  Append the axes on to the chart
+    chart.append('g')
+      .call(xAxis)
+    chart.append('g')
+      .call(yAxis)
+  }
+}
+
+export default LineChartComponent
